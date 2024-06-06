caede6407ac2af9e203edbfdee39a9c8b00fedcd
Aditya NV <adityanv4@gmail.com>
2024-06-06 21:55:45 +0530

    update in main.rs

--- src/hello.rs
+++ src/hello.rs
@@ -1,11 +1,16 @@
 //This is repo with helps to enhance your 
 //Knowledge in rust 
 
-//main function  
+//These are called modules in rust 
+//Which helps us to organize our code
 pub mod variables;
+pub mod structs;
+//main function  
+
 fn main() 
 { 
     //print function 
     variables::var();
+    structs::m();
     println!("Hello World");
-}
+}  
